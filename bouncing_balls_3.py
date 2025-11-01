312bbde377c38762f3f2164c97fb6e63eca78230
Thomas Viking Christiansson <160872481+ThomasCsson@users.noreply.github.com>
2025-11-01 20:05:29 +0100

    Update bouncing_balls_3.py

---
+++ bouncing_balls_3.py
@@ -0,0 +1,63 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+
+
+fig, ax = plt.subplots()
+ax.set_xlim(0, 10)
+ax.set_ylim(0, 10)
+
+ball_radius = 0.5
+ball1, = ax.plot([], [], 'o', markersize=ball_radius * 20, color='b')
+ball2, = ax.plot([], [], 'o', markersize=ball_radius * 20, color='r')
+
+
+x_pos1 = 3
+y_pos1 = 9
+y_vel1 = 0
+
+x_pos2 = 7
+y_pos2 = 7
+y_vel2 = 0
+
+gravity = -9.8
+time_step = 0.05
+elasticity = 0.8
+
+def init():
+    ball1.set_data([], [])
+    ball2.set_data([], [])
+    return ball1, ball2
+
+def update(frame):
+    global y_pos1, y_vel1, y_pos2, y_vel2
+
+    # Update velocity and position for ball 1 based on gravity
+    y_vel1 += gravity * time_step
+    y_pos1 += y_vel1 * time_step
+
+    # Ball 1 bounce
+    if y_pos1 - ball_radius < 0:
+        y_pos1 = ball_radius  # Prevent ball 1 from going below ground
+        y_vel1 = -y_vel1 * elasticity  # Reverse velocity for bounce
+
+    # Update velocity and position for ball 2 based on gravity
+    y_vel2 += gravity * time_step
+    y_pos2 += y_vel2 * time_step
+
+    # Ball 2 bounce
+    if y_pos2 - ball_radius < 0:
+        y_pos2 = ball_radius  # Prevent ball 2 from going below ground
+        y_vel2 = -y_vel2 * elasticity  # Reverse velocity for bounce
+
+    # Update ball positions
+    ball1.set_data(x_pos1, y_pos1)
+    ball2.set_data(x_pos2, y_pos2)
+
+    return ball1, ball2
+
+# Animation function
+ani = FuncAnimation(fig, update, frames=np.arange(0, 200), init_func=init, blit=True, interval=20)
+
+# Show animation
+plt.show()
